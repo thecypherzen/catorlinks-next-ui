e499242689827c8dbae3d049a67e5c9819b4ca33
William Inyam <sw.inyam@outlook.com>
2025-09-19 12:14:46 +0100

    :star: feat(ux): make carousel responsive

--- components/MainCarousel.tsx
+++ components/MainCarousel.tsx
@@ -20,36 +20,41 @@ export default function MainCarousel() {
       <CarouselContent className="-ml-1 w-full gap-5">
         {data.map((entry, idx) => {
           return (
-            <CarouselItem key={`carousel-item-${idx}`} className="basis-3/5">
+            <CarouselItem
+              key={`carousel-item-${idx}`}
+              className="basis-full sm:basis-4/5 lg:basis-3/5"
+            >
               <Card
                 className="rounded-2xl relative overflow-hidden shadow-none border-none bg-cover bg-center bg-no-repeat z-5"
                 style={{ backgroundImage: `url(${entry.image})` }}
               >
                 <div className="absolute inset-0 z-6 backdrop-blur-xs backdrop-opacity-50"></div>
                 <div className="h-full w-full absolute inset-0 z-7 bg-linear-to-b from-transparent to-neutral-950/90 mix-blend-multiply"></div>
-                <CardContent className="text-neutral-50 w-auto min-h-[300px] items-start justify-start z-20 grid grid-cols-1 grid-rows-[1fr_3fr_2fr] pt-0 px-6">
+                <CardContent className="text-neutral-50 w-auto min-h-[300px] items-start justify-start z-20 grid grid-cols-1  grid-rows-[0.5fr_1.5fr_2fr] sm:grid-rows-[1fr_2fr_2fr] lg:grid-rows-[1fr_3fr_2fr]pt-0 px-6">
                   {/* top left content */}
                   <h4 className="uppercase text-xs tracking-tight">
                     {entry.shortDesc}
                   </h4>
                   {/* main title */}
                   <div className="h-full flex flex-col items-center justify-center">
-                    <h3 className="text-5xl text-center font-bold flex flex-col items-center">
+                    <h3 className="text-4xl lg:text-5xl text-center font-extrabold md:font-bold flex flex-col items-center">
                       {entry.name}
                     </h3>
                   </div>
                   {/* lower content */}
-                  <div className="flex items-end justify-between gap-6">
-                    <div className="w-3/5 space-y-2">
-                      <h5 className="font-bold text-xl capitalize">
+                  <div className="flex w-full h-full flex-col items-center justify-end md:flex-row md:items-end md:justify-between gap-6">
+                    <div className="w-full md:w-3/5 space-y-2">
+                      <h5 className="text-center md:text-left font-bold text-xl capitalize">
                         {entry.title}
                       </h5>
-                      <p className="text-xs">{entry.longDesc}</p>
+                      <p className="text-xs text-center md:text-left">
+                        {entry.longDesc}
+                      </p>
                     </div>
-                    <div className="w-2/5 flex justify-end">
+                    <div className="w-full md:w-2/5 flex justify-center md:justify-end">
                       <Button
                         asChild
-                        className="rounded-full bg-neutral-50 cursor-pointer hover:bg-linear-to-br from-purple-300 to-blue-500 hover:scale-105 transition-scale duration-400 px-4 py-5 min-w-1/2 text-neutral-950 font-semibold"
+                        className="rounded-full bg-neutral-50 cursor-pointer active:bg-linear-to-br hover:bg-linear-to-br from-purple-300 to-blue-500 hover:scale-105 transition-scale duration-400 px-4 py-5 min-w-1/2 text-neutral-950 font-semibold"
                       >
                         <Link href="#">{`Try ${
                           entry.name.split(" ")[0]
