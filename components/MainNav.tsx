61b35ef122179a008532d302429bc544808da682
William Inyam <sw.inyam@outlook.com>
2025-09-19 14:54:45 +0100

    :star: feat(ux): complete mobile nav responsiveness

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -23,14 +23,25 @@ import { UseTheme } from "@/hooks/UseTheme";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 import { UseIsBreakpoint } from "@/hooks/UseIsBreakpoint";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import MenuNav from "./MenuNav";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export default function MainNav() {
   const { theme } = UseTheme();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   const isMobile = UseIsBreakpoint(932);
 
   useEffect(() => {}, [theme]);
+  useEffect(() => {}, [menuIsOpen]);
   return (
     <nav className="flex flex-col justify-center bg-background/70 sticky top-0 backdrop-blur-3xl z-50 backdrop-opacity-100 py-2 lg:py-3">
       <PageWrapper className="h-full">
@@ -62,10 +73,62 @@ export default function MainNav() {
               <div className="flex-1 translate-3d translate-z-0">
                 <MenuNav />
               </div>
-              <Sheet>
+              <Sheet open={menuIsOpen} onOpenChange={setMenuIsOpen}>
                 <SheetTrigger className="p-1 active:bg-neutral-300 dark:bg-neutral-900 rounded-sm">
                   <Menu strokeWidth="2px" className="text-xl" spacing="2px" />
                 </SheetTrigger>
+                <SheetContent className="gap-0 !w-3/7 border-1">
+                  <SheetHeader className="shadow-lg shadow-neutral-200/50 border-b-[0.3px] border-b-neutral-100/30 dark:border-b-neutral-600/30 dark:shadow-neutral-900/60 translate-3d translate-z-0">
+                    <SheetTitle className="flex py-4  divide-x-1 divide-foreground/60 gap-3 items-center">
+                      <div className="pr-3">
+                        <Logo />
+                      </div>
+                      <span className="text-foreground/60 uppercase">Menu</span>
+                    </SheetTitle>
+                    <SheetDescription className="sr-only">
+                      Navigation menu
+                    </SheetDescription>
+                  </SheetHeader>
+                  <div className="p-2 flex flex-col gap-6 justify-start items-center max-h-[calc(100%-100px)] overflow-y-auto sticky bottom-0">
+                    <CurrentUser className="justify-between w-full" />
+                    {/* Quick Actions */}
+                    <Card className="w-full gap-4">
+                      <CardHeader>
+                        <CardTitle className="mobile-section-header font-medium text-xs uppercase">
+                          Controls
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="space-y-2">
+                        <div className="flex justify-between align-center w-full px-3 py-2 rounded-md font-medium bg-foreground/5 hover:bg-foreground/10 text-foreground/80 active:bg-outline-1 active:outline-foreground/20 items-center cursor-pointer dark:hover:bg-foreground/20">
+                          <h6>Notifications</h6>
+                          <NotificationAlert forMobile />
+                        </div>
+                        <div className="flex justify-between align-center w-full px-3 py-2 rounded-md font-medium bg-foreground/5 hover:bg-foreground/10 text-foreground/80 active:bg-outline-1 active:outline-foreground/20 items-center cursor-pointer dark:hover:bg-foreground/20">
+                          <h6>Toggle Theme</h6>
+                          <ThemeToggle forMobile />
+                        </div>
+                      </CardContent>
+                    </Card>
+                    <Card className="w-full gap-4">
+                      <CardHeader>
+                        <CardTitle className="mobile-section-header font-medium text-xs uppercase">
+                          Quick Actions
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="space-y-2">
+                        <QuickNavButtons forMobile />
+                      </CardContent>
+                    </Card>
+                  </div>
+                  <SheetFooter>
+                    <div className="flex justify-between items-center">
+                      <AvatarPlaceholder />
+                      <span className="text-xs text-foreground/50">
+                        &copy; 2025 - <span>William Inyam</span>
+                      </span>
+                    </div>
+                  </SheetFooter>
+                </SheetContent>
               </Sheet>
             </>
           )}
@@ -98,11 +161,11 @@ function AvatarPlaceholder() {
   );
 }
 
-function CurrentUser() {
+function CurrentUser({ className, ...props }: React.ComponentProps<"div">) {
   const [open, setOpen] = useState<boolean>(false);
   const { theme } = UseTheme();
   return (
-    <div className="flex items-center">
+    <div className={cn("flex items-center", className)} {...props}>
       <Avatar className="size-7">
         <AvatarFallback
           className={cn(
@@ -142,7 +205,7 @@ function CurrentUser() {
   );
 }
 
-function NotificationAlert() {
+function NotificationAlert({ forMobile = false }: { forMobile?: boolean }) {
   const [ntfCount, setNtfCount] = useState<number>(0);
 
   useEffect(() => {
@@ -152,7 +215,12 @@ function NotificationAlert() {
 
   useEffect(() => {}, [ntfCount]);
   return (
-    <div className="hover:bg-foreground/10 rounded-full bg-foreground/5 p-1 size-7 flex flex-col items-center justify-center text-foreground/80 hover:text-foreground relative">
+    <div
+      className={cn(
+        "rounded-full size-8 flex flex-col items-center justify-center text-foreground/80 hover:text-foreground relative",
+        forMobile ? "nav-icon-mobile" : "nav-icon-desktop"
+      )}
+    >
       <Bell size="16px" />
       {ntfCount ? (
         <div className="absolute -top-0 right-0 size-2 bg-red-500 rounded-full flex flex-col items-center justify-center">
@@ -165,29 +233,61 @@ function NotificationAlert() {
   );
 }
 
-function QuickNavButtons() {
+function QuickNavButtons({ forMobile = false }: { forMobile?: boolean }) {
   return (
-    <div className="flex gap-2 items-cener justify-center">
-      <Button className="flex justify-center items-center gap-2 text-xs font-medium p-1 bg-foreground/5 hover:bg-foreground/10 text-foreground/80 rounded-xl">
-        <Images size="16px" />
-        <span>Gallery</span>
+    <div
+      className={cn(
+        "flex gap-2",
+        forMobile ? "flex-col" : "items-cener justify-center"
+      )}
+    >
+      <Button
+        className={cn(
+          "flex font-medium bg-foreground/5 active:bg-outline-1 active:outline-foreground/20 hover:bg-foreground/10 text-foreground/80  items-center cursor-pointer dark:hover:bg-foreground/20",
+          forMobile
+            ? "w-full justify-between px-3 py-2 h-auto"
+            : "justify-center gap-2 text-xs p-1 rounded-xl"
+        )}
+      >
+        <div
+          className={cn(
+            forMobile ? "nav-icon-mobile size-8 order-2" : "order1"
+          )}
+        >
+          <Images size="16px" />
+        </div>
+        <span className={forMobile ? "order-1" : "order-2"}>Gallery</span>
       </Button>
-      <Button className="flex justify-center items-center gap-2 text-xs font-medium p-1 bg-foreground/5 hover:bg-foreground/10 text-foreground/80 rounded-xl">
-        <Headset size="16px" />
-        <span>Support</span>
+      <Button
+        className={cn(
+          "flex font-medium bg-foreground/5 hover:bg-foreground/10 text-foreground/80 active:bg-outline-1 active:outline-foreground/20 items-center cursor-pointer dark:hover:bg-foreground/20",
+          forMobile
+            ? "w-full justify-between px-3 py-2 h-auto"
+            : "justify-center gap-2 text-xs p-1 rounded-xl"
+        )}
+      >
+        <div
+          className={cn(
+            forMobile ? "nav-icon-mobile size-8 order-2" : "order-1"
+          )}
+        >
+          <Headset size="16px" />
+        </div>
+        <span className={forMobile ? "order-1" : "order-2"}>Support</span>
       </Button>
     </div>
   );
 }
 
-function ThemeToggle() {
+function ThemeToggle({ forMobile = false }: { forMobile?: boolean }) {
   const { theme, toggleTheme } = UseTheme();
 
   return (
     <div
-      className={
-        "hover:bg-foreground/10 rounded-full bg-foreground/5 p-1 size-7 flex flex-col items-center justify-center text-foreground/80 hover:text-foreground"
-      }
+      className={cn(
+        "rounded-full size-8 flex flex-col items-center justify-center text-foreground/80 hover:text-foreground relative",
+        forMobile ? "nav-icon-mobile" : "nav-icon-desktop"
+      )}
       onClick={() => {
         toggleTheme();
       }}
